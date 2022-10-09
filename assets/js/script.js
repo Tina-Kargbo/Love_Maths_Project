9e211c535198077102ec294be5145e5dcc598a0e
Tina-Kargbo <cl.amankwaah@gmail.com>
2022-10-09 17:04:15 +0000

    add initial event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,17 @@
 
 document.addEventListener("DOMcontentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked $´) 
+            }
+        }
+    }
 })
 
 function runGame() {
